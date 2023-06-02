2df5d98ba4fc42604300f21c71a16b620228538f
junichikatsu <junichi.katsu@uhuru.jp>
2023-06-03 00:42:48 +0900

    製作例のスケッチを修正

--- examples/Production/EnvironmentCommenter/EnvironmentCommenter.ino
+++ examples/Production/EnvironmentCommenter/EnvironmentCommenter.ino
@@ -5,10 +5,10 @@
 #include "Seeed_BME280.h"
 #include <ArduinoJson.h>
 
-#define CHATGPT_TOKEN "chat gpt token"
+// ChatGPTのsecret keyを設定する
+#define CHATGPT_SECRET_KEY  "chargpt secret key"
 
-const char* chatGptToken = "Bearer " CHATGPT_TOKEN;
-
+// OLEDディスプレイのSH1107を使うための定義用のクラス
 class LGFX_SH1107 : public lgfx::LGFX_Device {
   lgfx::Panel_SH110x   _panel_instance;
   lgfx::Bus_I2C   _bus_instance;
@@ -17,18 +17,19 @@ class LGFX_SH1107 : public lgfx::LGFX_Device {
     LGFX_SH1107() {
       {
         auto cfg = _bus_instance.config();
-        cfg.i2c_port    = 1;
-        cfg.freq_write  = 400000;
+        cfg.i2c_port    = 1;      // I2Cのポート番号(Wire1)
+        cfg.freq_write  = 400000; 
         cfg.freq_read   = 400000;
-        cfg.pin_sda     = 6;
-        cfg.pin_scl     = 7;
-        cfg.i2c_addr    = 0x3C;
+        cfg.pin_sda     = 6;      // GroveシールドのI2C1が繋がっているピン番号
+        cfg.pin_scl     = 7;      // GroveシールドのI2C1が繋がっているピン番号
+        cfg.i2c_addr    = 0x3C;   // OLEDディスプレイのI2Cアドレス
 
         _bus_instance.config(cfg);
         _panel_instance.setBus(&_bus_instance);
       }
       {
         auto cfg = _panel_instance.config();
+        // OLEDディスプレイを横向きに使うための設定
         cfg.memory_width  = 64;
         cfg.memory_height = 128;
         cfg.offset_x      = 32;
@@ -45,52 +46,61 @@ BME280 bme280;
 ConnectWifi connect;
 static LGFX_SH1107 lcd;
 static LGFX_Sprite canvas(&lcd);
+const char* chatGptSecretkey = "Bearer " CHATGPT_SECRET_KEY;
 
-char* getEnvironmentalAssessmentForChatGPT(
-  float temperature,float humidity,float pressure
+String requestChatGPT(
+  const String message
 ){
+  String responseMessage ="取得できませんでした";
+
   if(connect.isConnected()){
     HTTPClient https;
     https.setInsecure();
     if (https.begin("https://api.openai.com/v1/chat/completions")) {
       https.addHeader("Content-Type", "application/json; charset=utf-8");
-      https.addHeader("Authorization", chatGptToken);
-      String message = "気温が" + String((int)temperature)
-        + "度、湿度が"
-        + String((int)humidity)
-        + "%、気圧が"
-        + String((int)pressure)
-        + "hPaです。どんな風にすごしたら良いかを40文字で教えて";
+      https.addHeader("Authorization", chatGptSecretkey);
       String body = 
         "{\"model\":\"gpt-3.5-turbo\",\"messages\":[{\"role\":\"user\",\"content\":\""
         + message
         + "\"}]}";
-      Serial.println("req:");
-      Serial.println(body);
       int status = https.POST(body.c_str());
-      String data = https.getString();
-      DynamicJsonDocument doc(1024);
-      DeserializationError error = deserializeJson(doc, data);
-      if (error) {
-        Serial.print("deserializeJson() failed: ");
-        Serial.println(error.c_str());
-        return NULL;
+      if (status > 0) {
+        String data = https.getString();
+        
+        DynamicJsonDocument doc(1024);
+        DeserializationError error = deserializeJson(doc, data);
+        if (!error) {
+          JsonObject choices_0 = doc["choices"][0];
+          responseMessage = String(choices_0["message"]["content"]);
+        }
       }
-      JsonObject choices_0 = doc["choices"][0];
-      const char* choices_0_message_content = choices_0["message"]["content"];
-      Serial.println("status:");
-      Serial.println(status);
-      Serial.println("res:");
-      Serial.println(data);
-      Serial.println("message:");
-      Serial.println(choices_0_message_content);
       https.end();
-
-      return (char*)choices_0_message_content;
     }
   }
 
-  return NULL;
+  return responseMessage;
+}
+
+String getEnvironmentalAssessmentForChatGPT(
+  float temperature,float humidity,float pressure
+){
+  String message = "気温が"
+    + String(temperature)
+    + "度、湿度が"
+    + String(humidity)
+    + "%、気圧が"
+    + String(pressure)
+    + "hPaです。どんな風にすごしたら良いかを30文字以内で教えて";
+
+  return requestChatGPT(message);
+}
+
+void display(const String dispayData,const lgfx::U8g2font* font){
+  canvas.fillScreen(TFT_BLACK);
+  canvas.setCursor(0, 0);
+  canvas.setFont(font);
+  canvas.println(dispayData);
+  canvas.pushSprite(0, 0);
 }
 
 void setup() {
@@ -105,9 +115,13 @@ void setup() {
   }
   // ディスプレイの初期設定
   lcd.init();
+  // 画面右で折り返す設定
   canvas.setTextWrap(true);
+  // 画面サイズでキャンバス用のメモリを確保
   canvas.createSprite(lcd.width(), lcd.height());
+  // 黒で塗りつぶし
   canvas.fillScreen(TFT_BLACK);
+  // 文字色は白
   canvas.setTextColor(TFT_WHITE);
 
   // Wi-Fiに接続
@@ -115,34 +129,26 @@ void setup() {
 }
 
 void loop() {
-  static bool displayMode = false;
-
   // BME280から環境情報を取得する
   float temperature = bme280.getTemperature();
   int humidity = (int)bme280.getHumidity();
   float pressure = bme280.getPressure()/100.0;
 
-  if(displayMode){
-    // Wi-Fiに接続されているかチェックする
-    if(connect.isConnected()){
-      canvas.fillScreen(TFT_BLACK);
-      canvas.setCursor(0, 0);
-      canvas.setFont(&fonts::lgfxJapanMincho_12);
-      canvas.println(getEnvironmentalAssessmentForChatGPT(temperature,(float)humidity,pressure));
-      canvas.pushSprite(0, 0);
-      delay(10000);
-      displayMode = false;
-    }
-  } else {
-    canvas.fillScreen(TFT_BLACK);
-    canvas.setCursor(0, 0);
-    canvas.setFont(&fonts::lgfxJapanMincho_20);
-    canvas.printf("温度:%.1f℃\n湿度:%d％\n気圧:%dhPa\n",temperature,humidity,(int)pressure);
-    canvas.pushSprite(0, 0);
-  }
-
   if(BOOTSEL){
-    displayMode = !displayMode;
+    display("問い合わせ中",&fonts::lgfxJapanMincho_20);
+    display(getEnvironmentalAssessmentForChatGPT(
+      temperature,
+      (float)humidity,
+      pressure
+      ),
+      &fonts::lgfxJapanMincho_16
+    );
+    delay(10000);
+  } else {
+    String displayData = "温度:" + String(temperature,1) + "℃\n";
+    displayData += "湿度:" + String(humidity) + "％\n";
+    displayData += "気圧:" + String(pressure,0) + "hPa\n";
+    display(displayData,&fonts::lgfxJapanMincho_20);
   }
 
   delay(100);
---
+++ examples/Production/keyReminder/keyReminder.ino
@@ -0,0 +1,111 @@
+#include "ConnectWifi.h"
+#include <HTTPClient.h>
+#include "HX711.h"
+
+// LINEアクセストークン
+#define LINE_ACCESS_TOKEN "gMkenNHvjKnU9gDbxwmeu4eZD4OB8mnBK2MPMqFcLLH"
+
+const char* lineAccessToken = "Bearer " LINE_ACCESS_TOKEN;
+
+// ロードセル用変換回路が接続されているピン
+const int HX711_DOUT_PIN = 19;
+const int HX711_SCK_PIN = 18;
+// 人感センサーが接続されているピン
+const int PIR_SENSOR_PIN = 20;
+
+ConnectWifi connect;
+HX711 scale;
+
+long baseScale = 0;
+
+void lineNotify(
+  const String message
+){
+  if(connect.isConnected()){
+    HTTPClient https;
+    https.setInsecure();
+    if (https.begin("https://notify-api.line.me/api/notify")) {
+      https.addHeader("Content-Type", "application/x-www-form-urlencoded");
+      https.addHeader("Authorization", lineAccessToken);
+      String body = "message=" + message;
+      int status = https.POST(body.c_str());
+      if (status == 200){
+        Serial.println("LINE通知完了");
+      } else {
+        Serial.printf("LINE通知エラー ステータス:%d", status);
+      }
+      https.end();
+    }
+  }
+}
+
+
+void setup() {
+  delay(1000);
+  
+  // 焦電センサー用のピンを入力に設定
+  pinMode(PIR_SENSOR_PIN, INPUT);
+  // 重量センサーの初期設定
+  scale.begin(HX711_DOUT_PIN, HX711_SCK_PIN);
+  if (scale.is_ready()) {
+    // 重量の基準を保持
+    baseScale = scale.read();
+  } else {
+    // 重量センサーの設定に失敗したら再起動する
+    Serial.println("HX711 initialization error!");
+    delay(10000);
+    rp2040.reboot();
+  }
+  // Wi-Fiに接続
+  connect.begin();
+}
+
+void loop() {
+  if (scale.is_ready()) {
+    bool keyStatus = false;
+    static bool pirSensingKeyStatus = false;
+    // 重量センサー値を読み込み
+    long reading = scale.read();
+    // 基準との差分を計算
+    long difference = abs(reading - baseScale);
+    // 人感センサーの状態を読み込み
+    int pirSensor = digitalRead(PIR_SENSOR_PIN);
+    // 人感センサーの前回値用
+    static int beforPirSensor = pirSensor;
+    // 人感センサーの状態でLEDを点灯
+    digitalWrite(LED_BUILTIN,!pirSensor);
+    // 鍵が置いてあるかどうかを判定
+    if(constrain(difference, -2000,2000) == difference){
+      keyStatus = false;
+    } else {
+      keyStatus = true;
+    }
+    // 人感センサーが反応したタイミングの鍵の状態を保持
+    if(beforPirSensor == LOW && pirSensor == HIGH){
+      pirSensingKeyStatus = keyStatus;
+    }
+    // 人感センサーの反応が無くなったタイミングで値を評価
+    if(beforPirSensor == HIGH && pirSensor == LOW){
+      if(pirSensingKeyStatus){
+        // 人感センサーが反応したときに鍵がある場合
+        if(!keyStatus){
+          // 鍵がなくなった
+          lineNotify("いってらっしゃい！");
+        }
+      } else {
+        // 人感センサーが反応したときに鍵が無い場合
+        if(keyStatus){
+          // 鍵が置かれた
+          lineNotify("お帰りなさい！");
+        } else {
+          // 鍵が置かれてない
+          lineNotify("鍵を置き忘れてない！？");
+        }
+      }
+    }
+    // 人感センサーの前回値を保持
+    beforPirSensor = pirSensor;
+  }
+
+  delay(1000);
+}
